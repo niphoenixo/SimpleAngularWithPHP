5738ef221c8360b7a0bb29f142a871defbea8d29
irreplaceable <33859704+niphoenixo@users.noreply.github.com>
2020-08-28 18:56:43 +0530

    Delete dbConfig.php

--- config/dbConfig.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Define database configuration
-define("DB_HOST", "localhost");
-define("DB_NAME", "graycover_png_lc");
-define("DB_CHARSET", "utf8");
-define("DB_USER", "root");
-define("DB_PASS", ""); 
-define("DB_ENGINE", "innodb");
-?>
